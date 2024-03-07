275f6604977cf85979f5fd08ac4b074e94003121
pedrolduarte13 <pedro.l.duarte@iesb.edu.br>
2024-03-07 19:41:59 -0300

    Save 1

---
+++ EstruturaDeDados.js
@@ -0,0 +1,11 @@
+//  ESTRUTURA DE DADOS
+
+var num = 2;
+
+if (num === 1) {
+    console.log('num é igual a 1');
+} else {
+    console.log('Num não é igual a 1, o valor de num é:' + num);
+}
+
+aaaa
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # EstruturaDeDados
+
+teste
