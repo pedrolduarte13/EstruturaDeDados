9d25d484f6cfd23b476b79fbbd98f4f29779e681
pedrolduarte13 <pedro.l.duarte@iesb.edu.br>
2024-03-14 20:49:39 -0300

    a

--- EstruturaDeDadosDia14do03.js
+++ EstruturaDeDadosDia14do03.js
@@ -54,5 +54,19 @@ var book = new Book('Estrutura de Dados', 406, '976 85 7522 553 0');
 
 book.printIsbn();
 
-// 
+// ARRAYS (Vetores unidimensionais e Matrizes bidimensionais e multidimensionais)
+
+// Um array é a estrutura de dados mais simples possivel em uma memoria
+
+// Um aaray armazena valores que são todos do mesmo tipo (homogêneo); sequencialmente (como uma lista de valores)
+
+// Embora o JavaScript nos permita criar arrays com valores de tipso distintos.
+
+const numbers = [1, 2, 3, [4, 5, 6,]];
+
+console.log(numbers.length);
+
+//
+
+const 
 
