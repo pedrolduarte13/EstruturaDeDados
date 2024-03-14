020d433780b35aff657fcb6f4d1b68e81fa8b657
pedrolduarte13 <pedro.l.duarte@iesb.edu.br>
2024-03-14 20:12:35 -0300

    a

--- EstruturaDeDadosDia14do03.js
+++ EstruturaDeDadosDia14do03.js
@@ -24,3 +24,35 @@ console.log(book.tittle);
 console.log('Este livro possui:  ', book.pages , '  páginas.');
 
 //
+
+function Book (tittle, pages, isbn) {
+    this.tittle = tittle;
+    this.pages = pages;
+    this.isbn = isbn;
+}
+
+var book = new Book('Estrutura de Dados', 406, '976 85 7522 553 0');
+
+Book.prototype.printTitle = function() {
+    console.log(this.tittle);
+};
+
+book.printTitle();
+
+//
+
+function Book (tittle, pages, isbn) {
+    this.tittle = tittle;
+    this.pages = pages;
+    this.isbn = isbn;
+    this.printIsbn = function() {
+        console.log('ISBN:  ' + this.isbn);
+    }
+}
+
+var book = new Book('Estrutura de Dados', 406, '976 85 7522 553 0');
+
+book.printIsbn();
+
+// 
+
---
+++ teste.js
@@ -0,0 +1,27 @@
+let idade = 23
+
+if(idade >= 18) {
+    console.log("Maior de idade");
+} else {
+    console.log("Menor de idade");
+}
+
+let curso = "ads";
+idade = 20;
+
+if(curso == "ads" && idade >= 18) {
+    console.log("Confirmado")
+} else {
+    console.log("Negado");
+}
+
+// 
+
+
+var i = 2003;
+while (i <= 2024) {
+    console.log(i);
+    ++i;
+}
+
+console.log(i)
